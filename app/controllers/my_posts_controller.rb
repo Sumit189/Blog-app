ef6d87fd04401ba9ef28d5a6a667f5edd5d61ce6
sumit189 <sumit.18.paul@gmail.com>
2021-11-12 13:48:10 +0530

    "added comment validation"

--- app/controllers/my_posts_controller.rb
+++ app/controllers/my_posts_controller.rb
@@ -2,7 +2,7 @@ class MyPostsController < ApplicationController
   before_action :authenticate_user!
   def all
     @posts = Post.where(:uid => current_user.id)
-    @latest_my_post = @posts.first
-    @my_other_posts =  @posts.paginate(:page => params[:page], :per_page => 3)
+    @latest_my_post = @posts.last
+    @my_other_posts =  @posts.order(:id).reverse_order.all.paginate(:page => params[:page], :per_page => 3)
   end
 end
