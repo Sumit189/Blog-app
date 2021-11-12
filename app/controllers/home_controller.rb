99ad445370cdee3db408638a1392e231bba382bd
sumit189 <sumit.18.paul@gmail.com>
2021-11-13 00:52:11 +0530

    added post date and author

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @posts = Post.all
     @other_posts = Post.order(:id).reverse_order.all.paginate(:page => params[:page], :per_page => 3)
     @latest_post = Post.last
   end
