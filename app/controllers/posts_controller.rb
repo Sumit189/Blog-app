87659b9204bd2c502936489ca51e0d86fe04bd39
sumit189 <sumit.18.paul@gmail.com>
2021-11-14 21:41:43 +0530

    fixed comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  include ActionView::Helpers::DateHelper
+
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
