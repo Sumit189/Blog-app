dbb6f96fcade5e902f940a240722d28cdfe4ac8c
sumit189 <sumit.18.paul@gmail.com>
2021-11-14 17:59:53 +0530

    validation for category

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 10 }
   validates :image, presence: true
+  validates :category, presence: true
 end
